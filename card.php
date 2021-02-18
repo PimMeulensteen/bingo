54cc8b0673f2ac6f61703025081ec2333d78652a
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2021-02-18 11:44:33 +0100

    remove unwanted files

--- card.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "hello world\n";
-    echo "generating card for ".$_POST["email"]."\n";
-    $command = "python3 main.py ".$_POST["email"];
-    $output = shell_exec($command);
-    echo $output;
-    echo "done\n";
-?>
